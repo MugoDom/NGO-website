50bb7ea9185908591ac4a13b742c7859d153d6f7
Dominic Mugo <njuemugodominic@gmail.com>
2020-10-21 20:20:02 +0300

    Remove the commented sections

--- js/help.js
+++ js/help.js
@@ -1,16 +1,16 @@
-filterSelection("all") // Execute the function and show all columns
+filterSelection("all") 
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("column");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+ 
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
 
-// Show filtered elements
+
 function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -22,7 +22,7 @@ function w3AddClass(element, name) {
   }
 }
 
-// Hide elements that are not selected
+
 function w3RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -35,7 +35,7 @@ function w3RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
+
 var btnContainer = document.getElementById("btn-container");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
